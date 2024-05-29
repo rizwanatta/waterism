d2b3090ef3e4b7e8e3106d37396294d01d6afd50
rizwanatta <rizwanatta@protonmail.com>
2024-05-29 13:11:43 +0500

    picker of date completed

--- src/ components/DatePickerModal.jsx
+++ src/ components/DatePickerModal.jsx
@@ -4,17 +4,20 @@ import Modal from "react-native-modal";
 import DateTimePicker from "react-native-ui-datepicker";
 import dayjs from "dayjs";
 
-export default function DatePickerModal() {
+export default function DatePickerModal({ isOpen, onClose }) {
   const [date, setDate] = useState(dayjs());
 
   return (
-    <Modal isVisible={true} className=" justify-end ">
+    <Modal isVisible={isOpen} className=" justify-end ">
       <View className="bg-white">
         <DateTimePicker
           mode="single"
           timePicker={true}
           date={date}
-          onChange={(params) => setDate(params.date)}
+          onChange={(params) => {
+            setDate(params.date);
+            onClose(params.date);
+          }}
         />
       </View>
     </Modal>
--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,22 +1,45 @@
-import { View, FlatList } from "react-native";
-import React from "react";
+import { View, FlatList, Text, Pressable } from "react-native";
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 import Glass from "../ components/glass";
 import DatePickerModal from "../ components/DatePickerModal";
 
 export default function Home() {
+  const [openDatePicker, setOpenDatePicker] = useState(false);
+  const [date, setDate] = useState("");
   const glassCountData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   const __renderItem = ({ item }) => <Glass />;
 
+  function onModalClose(pickedDate) {
+    setDate(pickedDate);
+    setOpenDatePicker(false);
+  }
+
+  function onCalPressed() {
+    if (openDatePicker === false) {
+      setOpenDatePicker(true);
+    } else {
+      setOpenDatePicker(false);
+    }
+  }
+
   return (
     <View className=" flex">
+      <Pressable onPress={onCalPressed}>
+        <View className="flex flex-row items-center self-end p-5">
+          <Ionicons name={"calendar"} size={30} color="orange" />
+          <Text>Date: {date} </Text>
+        </View>
+      </Pressable>
+
       <FlatList
         numColumns={3}
         data={glassCountData}
         renderItem={__renderItem}
       />
 
-      <DatePickerModal />
+      <DatePickerModal isOpen={openDatePicker} onClose={onModalClose} />
     </View>
   );
 }
