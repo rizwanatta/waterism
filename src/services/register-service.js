c66e08469e42ea9f112056f65f657e5dd9e1f07f
rizwan-atta-kap <r_atta-itkap@kw.com>
2024-06-10 11:15:31 +0500

    upload img and get download url

--- src/services/register-service.js
+++ src/services/register-service.js
@@ -2,7 +2,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, storage } from "../database/firebaseConfig";
 import { saveUserSession } from "./storage-service";
 import { imgToBlob } from "../lib/blobMaker";
-import { ref, uploadBytes } from "firebase/storage";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 function attemptToRegisterNewUser(email, password, img) {
   createUserWithEmailAndPassword(auth, email, password).then((response) => {
@@ -20,7 +20,12 @@ function attemptToUploadImg(img, userUniqueID) {
       storage,
       "profilePics/" + userUniqueID + "/profile_" + Math.random() + ".png"
     );
-    uploadBytes(storageRef, blobResponse);
+    uploadBytes(storageRef, blobResponse).then((response) => {
+      getDownloadURL(storageRef).then((url) => {
+        alert(url);
+        console.log(url);
+      });
+    });
   });
 }
 
